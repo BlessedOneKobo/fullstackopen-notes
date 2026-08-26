e9e0ec09bec2d898dace29d4c22edd3ea058ce97
Abel Kalu <59095867+BlessedOneKobo@users.noreply.github.com>
2026-08-26 15:19:41 +0100

    initial commit

---
+++ frontend/src/services/notes.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/api/notes";
+
+const getAll = () => {
+  return axios
+    .get(baseUrl)
+    .then((response) =>
+      response.data.concat({
+        id: 10000,
+        content: "This note is not saved on the server",
+        important: true,
+      }),
+    );
+};
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject).then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject).then((response) => response.data);
+};
+
+export default { getAll, create, update };
